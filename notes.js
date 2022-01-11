35a89738cd2963be1da3f5946f7bfdad0d017bcb
Hilary Shaner <hilaryshaner@gmail.com>
2022-01-11 15:08:00 -0600

    added two daily notes

---
+++ notes.js
@@ -0,0 +1,17 @@
+const notes = [
+    {
+        id: 1,
+        subject: "git",
+        date: "1/11/22",
+        feeling: "confused but hopeful",
+        timeSpent: 120
+    },
+    {
+        id: 2,
+        subject: "arrays",
+        date: "1/11/22",
+        feeling: "confident but learning more",
+        timeSpent: 30
+    }
+]
+console.log(notes)
