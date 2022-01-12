e13f1c82e77002935e487d707f181755e0ad35db
Hilary Shaner <hilaryshaner@gmail.com>
2022-01-12 10:43:24 -0600

    added search term

--- notes.js
+++ notes.js
@@ -3,14 +3,14 @@ const notes = [
         id: 1,
         subject: "git",
         date: "1/11/22",
-        feeling: "confused but hopeful",
+        feeling: "confused",
         timeSpent: 120
     },
     {
         id: 2,
         subject: "arrays",
         date: "1/11/22",
-        feeling: "confident but learning more",
+        feeling: "confident",
         timeSpent: 30
     }
 ]
@@ -19,20 +19,23 @@ const noteAboutToday = {
     id: 3,
     subject: "push()",
     date: "1/12/22",
-    feeling: "almost confident",
+    feeling: "hopeful",
     timeSpent: 30
 }
 
 notes.push(noteAboutToday)
 
+const searchTerm = "confident"
+
 for (const note of notes) {
+    if (note.feeling === searchTerm) {
     console.log(`Note ${note.id}
 ${note.date}
 I learned about ${note.subject}.
 I spent ${note.timeSpent} minutes working on it.
 I felt ${note.feeling}.
-    
     `)
+    }
 }
 
 //console.log(notes)
